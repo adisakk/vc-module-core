89cfb0c0a02dc4f1ed94e65ef9b94e878fb061ab
adisakk <adisak_kon@hotmail.com>
2019-10-10 22:35:53 +0700

    Check SKU is already taken before create an item.

--- VirtoCommerce.Domain/Catalog/Services/IItemService.cs
+++ VirtoCommerce.Domain/Catalog/Services/IItemService.cs
@@ -1,10 +1,12 @@
-﻿using VirtoCommerce.Domain.Catalog.Model;
+using VirtoCommerce.Domain.Catalog.Model;
 namespace VirtoCommerce.Domain.Catalog.Services
 {
     public interface IItemService
     {
         CatalogProduct GetById(string itemId, ItemResponseGroup respGroup, string catalogId = null);
         CatalogProduct[] GetByIds(string[] itemIds, ItemResponseGroup respGroup, string catalogId = null);
+        CatalogProduct GetByCode(string itemCode, ItemResponseGroup respGroup, string catalogId = null);
+        CatalogProduct[] GetByCodes(string[] itemCodes, ItemResponseGroup respGroup, string catalogId = null);
         CatalogProduct Create(CatalogProduct item);
         void Create(CatalogProduct[] items);
         void Update(CatalogProduct[] items);
